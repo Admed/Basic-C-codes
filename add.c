c766f29576366f984a6105581436db860faf8428
Ahmed <aa.adnanahmed2005@gmail.com>
2024-04-30 12:18:00 +0600

    initial commit

---
+++ add.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int adnanerdokan(int atta, int maoyda) {
+    return atta + maoyda;
+}
+
+int inputnao()
+{
+    int jetakeinputnibo;
+    printf("Enter a number: ");
+    scanf("%d",&jetakeinputnibo);
+}
+
+// int main()
+// {
+//     int a,b;
+//     printf("Enter first number:");
+//     scanf("%d",&a );
+//     // printf("\n");
+//     printf("Enter second number:");
+//     scanf("%d",&b);
+//     // printf("\n");
+//     printf("The sum is : %d\n",adnanerdokan(a,b));
+//     printf("sum of 5 and 2 is: %d",adnanerdokan(5,2));
+//     return 0;
+// }
+
+int main()
